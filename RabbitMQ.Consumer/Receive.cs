b74786dada7b1671070a9ef38381ca5b8c70770e
Thomaz Henrique <treimberg7@gmail.com>
2022-10-05 13:17:49 -0300

    Enviando e recebendo mensagens pelo Producer e Consumer.

---
+++ RabbitMQ.Consumer/Receive.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    public class Receive
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine($" [x] Received {message}");
+                };
+
+                channel.BasicConsume(
+                    queue: "hello",
+                    autoAck: true,
+                    consumer: consumer
+                );
+
+                Console.WriteLine(" Press [backspace] to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}

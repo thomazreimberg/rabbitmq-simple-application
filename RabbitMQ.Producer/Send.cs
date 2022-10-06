e1606111bf28856d8fa0ed238a9f471bc7846fa4
Thomaz Henrique <treimberg7@gmail.com>
2022-10-06 10:19:15 -0300

    Adding comments to Producer/Send class.

--- RabbitMQ.Producer/Send.cs
+++ RabbitMQ.Producer/Send.cs
@@ -8,23 +8,28 @@ namespace RabbitMQ.Producer
     {
         public static void Main()
         {
+            //Defining host
             var factory = new ConnectionFactory() { HostName = "localhost" };
 
+            //Creating connection with the defined host.
             using (var connection = factory.CreateConnection())
+
             using (var channel = connection.CreateModel())
             {
+                //Declaring/creating a queue
                 channel.QueueDeclare(
-                    queue: "hello",
-                    durable: false,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null
+                    queue: "hello", //Name of queue
+                    durable: false, //Define if this queue must survive when broker/producer is restarted
+                    exclusive: false, //Define if this queue use will be limited to the declaring connection. If yes, it will be deleted when declaring connection closes
+                    autoDelete: false, //Define if this queue be auto-deleted when its last consumer (if any) unsubscribes
+                    arguments: null //Additional queue arguments
                 );
 
                 string message = "Hello World!";
 
                 var body = Encoding.UTF8.GetBytes(message);
 
+                //Send the written message (message in bytes)
                 channel.BasicPublish(
                     exchange: "",
                     routingKey: "hello",
